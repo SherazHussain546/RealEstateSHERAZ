1d4a477ca8c4f774cbb79a7a398476c3e7f9d8ad
sherazhussain546 <sherazhussainofficial1@gmail.com>
2025-03-20 03:26:27 +0000

    Fix dynamic require issue using ESM imports

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+import path from 'path';
+
 app.use((req, res, next) => {
   const start = Date.now();
   const path = req.path;
